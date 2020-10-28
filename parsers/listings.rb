3d7f66d1447b2e42947ffe4ffee998106ad18907
kgenas <kKangenas@gmail.com>
2020-10-28 09:45:37 -0600

    modified listings

--- parsers/listings.rb
+++ parsers/listings.rb
@@ -2,26 +2,19 @@
 nokogiri = Nokogiri.HTML(content)
 
 # get the group of listings
-listings = nokogiri.css('ul.b-list__items_nofooter li.s-item')
+listings = nokogiri.css('.b-list__items_nofooter .s-item')
 
 # loop through the listings
 listings.each do |listing|
-    # initialize an empty hash
-    product = {}
 
-    # extract the information into the product hash
-    product['title'] = listing.at_css('h3.s-item__title')&.text
-
-    # extract the price
-    product['price'] = listing.at_css('.s-item__price')&.text
-
-    # extract the listing URL
-    item_link = listing.at_css('a.s-item__link')
-    product['url'] = item_link['href'] unless item_link.nil?
-
-    # specify the collection where this record will be stored
-    product['_collection'] = "listings"
-
-    # save the product to the job’s outputs
+    title = listing.css('.s-item__title').text.strip
+    price = listing.css('.s-item__price').text.strip
+    url = listing.css('.s-item__link').attr('href')
+    product = {
+        url: url,
+        page_type: 'listings',
+        title: title,
+        price: price  
+    }
     outputs << product
 end
